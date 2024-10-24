8e93e08e6e4542859dfccbd184da37a71138bc8a
Shane Galvin <Sgalvin36@gmail.com>
2024-10-24 15:32:42 -0600

    Add initial detailed tests

--- cypress/e2e/details.cy.js
+++ cypress/e2e/details.cy.js
@@ -1,13 +1,28 @@
-describe('Main Page', () => {
+describe('Details Page', () => {
     beforeEach(() => {
-        cy.intercept('GET', 'https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/155', {
+        cy.intercept('GET', 'https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies', {
             statusCode: 200,
             fixture: 'movie_posters'
         })
-        cy.visit('http://localhost:3000/155')
+
+        cy.intercept('GET', 'https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/155', {
+            statusCode: 200,
+            fixture: 'movie_details'
+        })
+
+        cy.visit('http://localhost:3000/')
+        cy.get('.movie-card').first().find('.movieImage').click()
     })
 
     it('displays title on page load', () => {
         cy.get('h1').contains('The RT Files')
+    })
+
+    it('displays the movie details', () =>{
+        cy.get('.movie-info > img').should('exist')
+        cy.get('.movie-info > .list-container').should('exist')
+        cy.get('.movie-info > h2').should('exist')
+        cy.get('.movie-info > p').should('exist')
+        cy.get('.movie-info > .extra_info').should('exist')
     })
 })
